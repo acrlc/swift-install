b12ae822d6a0cb9bfbc8eca85a19d05d5660f9ee
William Luke <williamluke@me.com>
2023-12-04 17:59:17 -0600

    update tag

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
  name: "swift-install", platforms: [.macOS("13.3")],
  products: [.executable(name: "swift-install", targets: ["Install"])],
  dependencies: [
-  .package(url: "https://github.com/codeAcrylic/shell.git", branch: "main")
+  .package(url: "https://github.com/acrlc/shell.git", branch: "main")
  ],
  targets: [
   .executableTarget(
